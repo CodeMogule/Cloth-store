4dbd498b6be23ba776535c986a43b809c562dbe3
CodeMogule <codemogule@gmail.com>
2021-04-27 17:38:10 +0300

    Cloth Store

--- Source/Js/app.js
+++ Source/Js/app.js
@@ -1,1 +1,42 @@
 
+const btn = document.querySelectorAll(".main-nav li")
+const product = document.querySelectorAll('.products')
+
+
+
+for(let x = 0 ; btn.length>x ; x++){
+    btn[x].addEventListener("click",function(ele){
+
+        ele.preventDefault()
+        const filtering = ele.target.dataset.filter;
+        //console.log(filtering)
+
+        product.forEach((eProduct) => {
+            if(filtering === 'all'){
+                eProduct.style.display = 'block'
+            } else if (eProduct.classList.contains(filtering)){
+                eProduct.style.display = 'block'
+            }else{
+                eProduct.style.display = 'none'
+            }
+        })
+    })
+}
+
+const textBox = document.querySelector('.search input')
+
+textBox.addEventListener('keyup',function(ele){
+const searchVal = ele.target.value.toLowerCase().trim()
+
+product.forEach((items)=>{
+if(items.textContent.includes(searchVal)){
+    items.style.display = 'block'
+} else if(searchVal === ''){
+    items.style.display = 'block'
+}
+else{
+    items.style.display = 'none'
+}
+})
+})
+
